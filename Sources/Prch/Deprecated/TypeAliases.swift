f53b624b1220aed9e18674f189cf48436f8fb723
Leo Dion <leogdion@brightdigit.com>
2022-02-12 10:58:18 -0500

    adding deprecated type aliases

---
+++ Sources/Prch/Deprecated/TypeAliases.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+@available(*, deprecated, renamed: "Client")
+public typealias APIClient = Client
+
+@available(*, deprecated, renamed: "ClientError")
+public typealias APIClientError = ClientError
