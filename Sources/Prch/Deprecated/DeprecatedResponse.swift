2725e9301c73380aef15c514f2b2f085c1797a0e
Leo Dion <leogdion@brightdigit.com>
2022-07-06 10:09:51 -0400

    updating build

--- Sources/Prch/Deprecated/DeprecatedResponse.swift
+++ Sources/Prch/Deprecated/DeprecatedResponse.swift
@@ -6,25 +6,3 @@ public protocol DeprecatedResponse: Response {
 
   var failure: FailureType? { get }
 }
-
-public extension DeprecatedResponse {
-  var decoded: ResponseResult<SuccessType, FailureType> {
-    if let successValue = success {
-      return .success(successValue)
-    } else if let failureValue = failure {
-      return .defaultResponse(statusCode, failureValue)
-    } else {
-      preconditionFailure()
-    }
-  }
-
-  var response: ClientResult<SuccessType, FailureType> {
-    decoded.response
-  }
-}
-
-public extension DeprecatedResponse where FailureType == Never {
-  var failure: FailureType? {
-    nil
-  }
-}
