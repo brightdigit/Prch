517608703fde10ef3ce65b3e664ecb01bc1a8d95
Leo Dion <leogdion@brightdigit.com>
2022-07-06 10:32:26 -0400

    Unhide dev dependencies

--- Package@swift-5.5.swift
+++ Package@swift-5.5.swift
@@ -14,11 +14,11 @@ let package = Package(
     .library(name: "Prch", targets: ["Prch"])
   ],
   dependencies: [
-//    .package(url: "https://github.com/shibapm/Komondor", from: "1.1.0"), // dev
-//    .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.47.0"), // dev
-//    .package(url: "https://github.com/realm/SwiftLint", from: "0.43.0"), // dev
-//    .package(url: "https://github.com/shibapm/Rocket.git", from: "1.2.0"), // dev
-//    .package(url: "https://github.com/brightdigit/swift-test-codecov", from: "1.0.0") // dev
+    .package(url: "https://github.com/shibapm/Komondor", from: "1.1.0"), // dev
+    .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.47.0"), // dev
+    .package(url: "https://github.com/realm/SwiftLint", from: "0.43.0"), // dev
+    .package(url: "https://github.com/shibapm/Rocket.git", from: "1.2.0"), // dev
+    .package(url: "https://github.com/brightdigit/swift-test-codecov", from: "1.0.0") // dev
   ],
   targets: [
     .target(name: "Prch"),
