65715b6deb412484451c6a9e225c8ee73e634487
Leo Dion <leogdion@brightdigit.com>
2023-04-12 08:28:48 -0400

    Step 1. Adding Generic Service

---
+++ Sources/Prch/Generic/GenericService.swift
@@ -0,0 +1,97 @@
+import Foundation
+import PrchModel
+
+#if canImport(FoundationNetworking)
+  import FoundationNetworking
+#endif
+
+class GenericService {
+  internal init(baseURLComponents: URLComponents, credentialsContainer: SimpleCredContainer, session: URLSession, headers: [String: String]) {
+    self.baseURLComponents = baseURLComponents
+    self.credentialsContainer = credentialsContainer
+    self.session = session
+    self.headers = headers
+  }
+
+  private let baseURLComponents: URLComponents
+  private let credentialsContainer: SimpleCredContainer
+  private let session: URLSession
+  private let headers: [String: String]
+
+  private static func headers(
+    withCredentials credentialsContainer: SimpleCredContainer?,
+    mergedWith headers: [String: String]
+  ) async throws -> [String: String] {
+    let creds = try await credentialsContainer?.fetch()
+
+    let authorizationHeaders: [String: String]
+    if let creds = creds {
+      authorizationHeaders = creds.httpHeaders
+    } else {
+      authorizationHeaders = [:]
+    }
+
+    return headers.merging(authorizationHeaders) { _, rhs in
+      rhs
+    }
+  }
+
+  private func build<RequestType: GenericRequest>(request: RequestType,
+                                                  withBaseURL baseURLComponents: URLComponents,
+                                                  withHeaders headers: [String: String]) throws -> URLRequest {
+    var componenents = baseURLComponents
+    componenents.path = "/\(request.path)"
+    componenents.queryItems = request.parameters.map(URLQueryItem.init)
+
+    guard let url = componenents.url else {
+      preconditionFailure()
+    }
+
+    var urlRequest = URLRequest(url: url)
+
+    urlRequest.httpMethod = request.method
+
+    let allHeaders = headers.merging(request.headers, uniquingKeysWith: { lhs, _ in lhs })
+
+    for (field, value) in allHeaders {
+      urlRequest.addValue(value, forHTTPHeaderField: field)
+    }
+
+    if let body = request.body {
+      urlRequest.httpBody = body
+    }
+
+    return urlRequest
+  }
+
+  func request<
+    RequestType: GenericRequest,
+    SuccessType: Decodable
+  >(
+    _ request: RequestType
+  ) async throws -> SuccessType {
+    let sessionRequest: URLRequest
+    let credetials = request.requiresCredentials ? credentialsContainer : nil
+
+    let headers = try await Self.headers(
+      withCredentials: credetials,
+      mergedWith: headers
+    )
+
+    sessionRequest = try build(
+      request: request,
+      withBaseURL: baseURLComponents,
+      withHeaders: headers
+    )
+
+    let (data, urlResponse) = try await session.data(for: sessionRequest)
+    guard let response = urlResponse as? HTTPURLResponse else {
+      throw RequestError.invalidResponse(urlResponse)
+    }
+    guard response.statusCode / 100 == 2 else {
+      throw RequestError.invalidStatusCode(response.statusCode)
+    }
+
+    return try JSONDecoder().decode(SuccessType.self, from: data)
+  }
+}
